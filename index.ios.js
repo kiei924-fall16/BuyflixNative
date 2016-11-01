2a5a781955c880a7050c72e0cf2af0a60d8ef446
Brian Eng <brian@brianeng.com>
2016-11-01 12:38:30 -0500

    use a listview

--- index.ios.js
+++ index.ios.js
@@ -5,8 +5,9 @@ var React = require('react')
 var ReactNative = require('react-native')
 
 // React Native components you want to use
-var { AppRegistry, Image, ScrollView, StyleSheet, Text, TouchableHighlight, View } = ReactNative
+var { AppRegistry, Image, ListView, StyleSheet, Text, TouchableHighlight, View } = ReactNative
 
+// Firebase
 var Firebase = require('firebase')
 var firebase = Firebase.initializeApp({
   databaseURL: "https://buyflix-d7037.firebaseio.com/"
@@ -16,73 +17,105 @@ var moviesRef = database.ref('/movies')
 
 var Movie = React.createClass({
   onMovieClicked: function() {
-    this.props.onMovieClicked(this.props.id)
+    this.props.onMovieClicked(this.props.movie)
   },
   render: function() {
     return (
-      <TouchableHighlight onPress={this.onMovieClicked} style={styles.thumbnail}>
-        <Image source={{uri: this.props.movie.poster}}
-               style={styles.thumbnail}
-               onClick={this.onMovieClicked} />
-      </TouchableHighlight>
+      <View style={styles.movieContainer}>
+        <TouchableHighlight style={styles.movie} onPress={this.onMovieClicked}>
+          <Image source={{uri: this.props.movie.poster}}
+                 style={styles.thumbnail} />
+        </TouchableHighlight>
+        <View style={styles.movie}>
+          <Text style={styles.movieTitle}>{this.props.movie.title}</Text>
+          <Text style={styles.movieText}>{this.props.movie.rating}</Text>
+          <Text style={styles.movieText}>{this.props.movie.genre}</Text>
+          <Text style={styles.movieText}>{this.props.movie.runtime}</Text>
+        </View>
+      </View>
     )
   }
 })
 
 var BuyflixNative = React.createClass({
-  onMovieClicked: function(movieId) {
-    var moviesWithoutRemoved = this.state.movies.filter(function(movie) {
-      return movie.key !== movieId
+  onMovieClicked: function(movie) {
+    var moviesWithoutRemoved = this.state.movies.filter(function(existingMovie) {
+      return existingMovie.id !== movie.id
     })
     this.setState({movies: moviesWithoutRemoved})
     moviesRef.set(moviesWithoutRemoved)
   },
   componentDidMount: function() {
     moviesRef.on('value', function(snapshot) {
-      this.setState({ movies: snapshot.val() })
+      this.setState({ 
+        movies: snapshot.val(),
+        dataSource: this.state.dataSource.cloneWithRows(snapshot.val())
+      })
     }.bind(this))
   },
   getInitialState: function() {
     return {
-      movies: []
+      movies: [],
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
     }
   },
   renderMovie: function(movie) {
-    return <Movie key={movie.key} movie={movie} id={movie.key} onMovieClicked={this.onMovieClicked} />
+    return <Movie key={movie.key} movie={movie} onMovieClicked={this.onMovieClicked} />
   },
   render: function() {
-    if (this.state.movies == null) {
-      return (
-        <View style={styles.noMovies}><Text>No movies!</Text></View>
-      )
-    } else {
-      return (
-        <View style={styles.scrollView}>
-          {this.state.movies.map(this.renderMovie)}
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Buyflix</Text>
         </View>
-      )
-    }
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderMovie}
+          style={styles.listView}
+        />
+      </View>
+    )
   }
 })
 
 var styles = StyleSheet.create({
-  noMovies: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#181818'
   },
-  scrollView: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 20,
-    backgroundColor: '#000000',
-    height: 1000
+  header: {
+    paddingTop: 30,
+    paddingBottom: 30
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 40,
+    fontFamily: 'Marker Felt',
+    color: '#fff'
+  },
+  listView: {
+    paddingTop: 20
+  },
+  movieContainer: {
+    padding: 0,
+    flexDirection: 'row'
+  },
+  movie: {
+    padding: 10
+  },
+  movieTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  movieText: {
+    color: '#fff'
   },
   thumbnail: {
-    width: 70,
-    height: 104,
-    margin: 5
+    width: 80,
+    height: 119
   }
 })
 
